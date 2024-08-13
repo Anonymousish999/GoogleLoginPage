71de401aff526a9585b65d9204107e431465422f
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-13 14:41:18 +0530

    Add: made password page and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { PasswordComponent } from './password/password.component';
 
 const routes: Routes = [
-  { path: '', component: RegisterComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: LoginComponent},
+  { path: 'password', component: PasswordComponent}
   // other routes
 ];
 
