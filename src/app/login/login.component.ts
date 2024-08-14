c04a8b0fb87c8c1b166e811af915fb0af75f3ef3
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-14 14:43:33 +0530

    Add: add validation feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
 
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Handle form submission
+    }
+  }
 }
